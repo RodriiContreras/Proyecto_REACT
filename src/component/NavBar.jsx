03d77a8f099d64b4e78991b2694eb8f550888ee0
rodriiContreras <rodrigitocontre@hotmail.com>
2021-07-12 20:59:30 -0300

    Saco Boostrap por renderizacion

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -2,30 +2,39 @@ import React from 'react'
 
 function NavBar() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Gamers</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Productos</a>
-              </li>
-              <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Sucursales</a>
-              </li>
-              <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#" >Contactanos</a>
-              </li>
-            </ul>
-          </div>
-        </div>
+      // <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      //   {/* <div class="container-fluid"> */}
+      //     {/* <a class="navbar-brand" href="#">Gamers</a> */}
+      //     {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"> */}
+      //       {/* <span class="navbar-toggler-icon"></span> */}
+      //     {/* </button> */}
+      //     {/* <div class="collapse navbar-collapse" id="navbarNav"> */}
+      //       {/* <ul class="navbar-nav"> */}
+      //         {/* <li class="nav-item"> */}
+      //           {/* <a class="nav-link active" aria-current="page" href="#">Home</a> */}
+      //         {/* </li> */}
+      //         {/* <li class="nav-item"> */}
+      //           {/* <a class="nav-link active" aria-current="page" href="#">Productos</a> */}
+      //         {/* </li> */}
+      //         {/* <li class="nav-item"> */}
+      //         {/* <a class="nav-link active" aria-current="page" href="#">Sucursales</a> */}
+      //         {/* </li> */}
+      //         {/* <li class="nav-item"> */}
+      //         {/* <a class="nav-link active" aria-current="page" href="#" >Contactanos</a> */}
+      // //         </li>
+      // //       </ul>
+      // //     </div>
+      // //   </div>
+      // // </nav>
+      <nav>
+         <h1>Gamers</h1>
+        <a href="#">Sucursales</a>
+        <a href="#">Productos</a>
+        <a href="#">Sucursales</a>
+        <a href="#">Contactanos</a>
+         
       </nav>
+
     )
 }
 
