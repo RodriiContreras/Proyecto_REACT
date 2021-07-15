d291802b1ea864666f1da188ec9f61048a995381
rodriiContreras <rodrigitocontre@hotmail.com>
2021-07-14 22:44:36 -0300

    cambios en componentes

--- src/component/ItemListContainer.jsx
+++ src/component/ItemListContainer.jsx
@@ -4,7 +4,7 @@ function ItemListContainer() {
     let nombre = 'Hola Mundo'
     return (
         <div>
-            <h1>{nombre}</h1>
+            <h1 style={{fontSize:'40px'}}>{nombre}</h1>
         </div>
     )
 }
