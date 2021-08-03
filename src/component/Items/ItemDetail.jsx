17e2f3cfc9cdc0d7554e80eed18cba2e7f65b6a5
rodriiContreras <rodrigitocontre@hotmail.com>
2021-08-03 01:06:58 -0300

    cambios

--- src/component/Items/ItemDetail.jsx
+++ src/component/Items/ItemDetail.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
 import ItemCount from '../ItemCount/ItemCount'
 import {useState} from 'react'
+import { Link } from 'react-router-dom'
 
 function ItemDetail({listaCervezas}) {
     const [cantidad, setCantidad] = useState(0)
-   const contador= (cantidades) =>{
+  
+    const Contador= (cantidades) =>{
     setCantidad(cantidades)
     evento()
     }
     function evento(){
-        let boton = document.getElementById('boton')  
-        boton.addEventListener('mouseover',function(){
-            boton.textContent='Finalizar mi pedido'
-            
+        const DOM = document.getElementById('boton')
+        DOM.addEventListener('click',function(){
+            DOM.style.visibility=('hidden')
+            if(DOM.visibility='hidden'){
+             let botonNuevo =   document.getElementById('divLink')
+             botonNuevo.style.visibility=('visible')
+            }
         })
+    }
+
+    function Click (){
+   return(
+       <div id='divLink' style={{position:'relative',bottom:'70px',left:'170px',width:'200px',height:'40px',visibility:'hidden',backgroundColor:'rgba(237,176,38,255)',zIndex:'1000'}}>
+       <Link style={{color:'black',textDecoration:'none'}} id='link' to='/cart'>Finalizar Mi Pedido</Link>
+       </div>
+   )
     }
     console.log(cantidad)
 
@@ -27,7 +40,9 @@ function ItemDetail({listaCervezas}) {
                 <div><p> al precio de ${cervezas.boil_volume.value}</p></div>
                 <p style={{fontWeight:'bolder'}}>{cervezas.description}</p>
                 <div style={{position:'relative',right:'70px',bottom:'20px'}}>
-                <ItemCount botonId='boton' stock={10} inicial={1} carrito={contador}/>
+                <ItemCount botonId='boton' stock={10} inicial={1} carrito={Contador}/>
+                <Click/>
+
                 </div>
             </div>))}
        </div>
