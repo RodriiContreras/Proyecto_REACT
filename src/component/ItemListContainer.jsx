648fdb3a6d31ffec6ea52d3896f89d2690ee130c
rodriiContreras <rodrigitocontre@hotmail.com>
2021-08-05 14:19:21 -0300

    cambios en items

--- src/component/ItemListContainer.jsx
+++ src/component/ItemListContainer.jsx
@@ -13,18 +13,8 @@ import TripleCheeseBurger from './imagen/TripleCheeseBurger.jpg'
 const contador= (cantidad) =>{
     console.log(cantidad)
 }
-function producto1(){
-   setTimeout(()=>{
-  let div =document.getElementById('contenedorcount1')
-div.style.visibility=('visible')
-},500)
-}
-function producto2(){
-  setTimeout(()=>{
- let div =document.getElementById('contenedorcount2')
-div.style.visibility=('visible')
-},500)
-}
+
+
 const items =[
     {id:'1',
     titulo:'Burger House',
@@ -50,8 +40,8 @@ const items =[
     categoria:'Vegano',
     imagen:VeganBurger,
     descripcion:'awkflawjfwakfwa',
-    link:<Link to='/productos/3'>Mas detalles</Link>,
-    Itemcount:<ItemCount stock={10} inicial={1} carrito={contador}/>
+    link:<Link onClick={producto3} to='/productos/3'>Mas detalles</Link>,
+    Itemcount:<div style={{visibility:'hidden'}} id='contenedorcount3'><ItemCount stock={10} inicial={1} carrito={contador}/></div>
     },
     {id:'4',
     titulo:'Double Bacon',
@@ -59,8 +49,8 @@ const items =[
     categoria:'Carne',
     imagen:Bacon,
     descripcion:'awkflawjfwakfwa',
-    link:<Link to='/productos/4'>Mas detalles</Link>,
-    Itemcount:<ItemCount stock={10} inicial={1} carrito={contador}/>
+    link:<Link onClick={producto4} to='/productos/4'>Mas detalles</Link>,
+    Itemcount:<div style={{visibility:'hidden'}} id='contenedorcount4'><ItemCount stock={10} inicial={1} carrito={contador}/></div>
     },
     {id:'5',
     titulo:'Tapa Arterias',
@@ -68,8 +58,8 @@ const items =[
     categoria:'Carne',
     imagen:CheeseBurger,
     descripcion:'awkflawjfwakfwa',
-    link:<Link to='/productos/5'>Mas detalles</Link>,
-    Itemcount:<ItemCount stock={10} inicial={1} carrito={contador}/>
+    link:<Link onClick={producto5} to='/productos/5'>Mas detalles</Link>,
+    Itemcount:<div style={{visibility:'hidden'}} id='contenedorcount5'><ItemCount stock={10} inicial={1} carrito={contador}/></div>
     },
     {id:'6',
     titulo:'Old School Burger',
@@ -77,8 +67,8 @@ const items =[
     imagen:OldSchool,
     precio:550,
     descripcion:'awkflawjfwakfwa',
-    link:<Link to='/productos/6'>Mas detalles</Link>,
-    Itemcount:<ItemCount stock={10} inicial={1} carrito={contador}/>
+    link:<Link onClick={producto6} to='/productos/6'>Mas detalles</Link>,
+    Itemcount:<div style={{visibility:'hidden'}} id='contenedorcount6'><ItemCount stock={10} inicial={1} carrito={contador}/></div>
     },
     {id:'7',
     titulo:'Triple CheeseBurger',
@@ -86,8 +76,8 @@ const items =[
     categoria:'Carne',
     imagen:TripleCheeseBurger,
     descripcion:'awkflawjfwakfwa',
-    link:<Link to='/productos/7'>Mas detalles</Link>,
-    Itemcount:<ItemCount stock={10} inicial={1} carrito={contador}/>
+    link:<Link onClick={producto7} to='/productos/7'>Mas detalles</Link>,
+    Itemcount:<div style={{visibility:'hidden'}} id='contenedorcount7'><ItemCount stock={10} inicial={1} carrito={contador}/></div>
     },
     {id:'8',
     titulo:'Old School Burger',
@@ -95,8 +85,8 @@ const items =[
     categoria:'Carne',
     imagen:CheeseBurger,
     descripcion:'awkflawjfwakfwa',
-    link:<Link to='/productos/8'>Mas detalles</Link>,
-    Itemcount:<ItemCount stock={10} inicial={1} carrito={contador}/>
+    link:<Link onClick={producto8} to='/productos/8'>Mas detalles</Link>,
+    Itemcount:<div style={{visibility:'hidden'}} id='contenedorcount8'><ItemCount stock={10} inicial={1} carrito={contador}/></div>
     },
     {id:'9',
     titulo:'Double Bacon',
@@ -104,8 +94,8 @@ const items =[
     categoria:'Carne',
     imagen:CheeseBurger,
     descripcion:'awkflawjfwakfwa',
-    link:<Link to='/productos/9'>Mas detalles</Link>,
-    Itemcount:<ItemCount stock={10} inicial={1} carrito={contador}/>
+    link:<Link onClick={producto9} to='/productos/9'>Mas detalles</Link>,
+    Itemcount:<div style={{visibility:'hidden'}} id='contenedorcount9'><ItemCount stock={10} inicial={1} carrito={contador}/></div>
     },
     {id:'10',
     titulo:'Veggie Burger',
@@ -113,8 +103,8 @@ const items =[
     categoria:'Vegano',
     imagen:CheeseBurger,
     descripcion:'awkflawjfwakfwa',
-    link:<Link to='/productos/10'>Mas detalles</Link>,
-    Itemcount:<ItemCount stock={10} inicial={1} carrito={contador}/>
+    link:<Link onClick={producto10} to='/productos/10'>Mas detalles</Link>,
+    Itemcount:<div style={{visibility:'hidden'}} id='contenedorcount10'><ItemCount stock={10} inicial={1} carrito={contador}/></div>
     },
     {id:'11',
     titulo:'Veggie Burger',
@@ -122,8 +112,8 @@ const items =[
     categoria:'Vegano',
     imagen:CheeseBurger,
     descripcion:'awkflawjfwakfwa',
-    link:<Link to='/productos/11'>Mas detalles</Link>,
-    Itemcount:<ItemCount stock={10} inicial={1} carrito={contador}/>
+    link:<Link onClick={producto11} to='/productos/11'>Mas detalles</Link>,
+    Itemcount:<div style={{visibility:'hidden'}} id='contenedorcount11'><ItemCount stock={10} inicial={1} carrito={contador}/></div>
     },
     {id:'12',
     titulo:'Veggie Burger',
@@ -131,10 +121,82 @@ const items =[
     categoria:'Vegano',
     imagen:CheeseBurger,
     descripcion:'awkflawjfwakfwa',
-    link:<Link to='/productos/12'>Mas detalles</Link>,
-    Itemcount:<ItemCount stock={10} inicial={1} carrito={contador}/>
+    link:<Link onClick={producto12} to='/productos/12'>Mas detalles</Link>,
+    Itemcount:<div style={{visibility:'hidden'}} id='contenedorcount12'><ItemCount stock={10} inicial={1} carrito={contador}/></div>
     }
   ]
+  function producto1(){
+    setTimeout(()=>{
+   let div =document.getElementById('contenedorcount1')
+  div.style.visibility=('visible')
+  },500)
+  }
+  function producto2(){
+   setTimeout(()=>{
+  let div =document.getElementById('contenedorcount2')
+  div.style.visibility=('visible')
+  },500)
+  }
+  function producto3(){
+    setTimeout(()=>{
+   let div =document.getElementById('contenedorcount3')
+   div.style.visibility=('visible')
+   },500)
+   }
+   function producto4(){
+    setTimeout(()=>{
+   let div =document.getElementById('contenedorcount4')
+   div.style.visibility=('visible')
+   },500)
+   }
+   function producto5(){
+    setTimeout(()=>{
+   let div =document.getElementById('contenedorcount5')
+   div.style.visibility=('visible')
+   },500)
+   }
+   function producto6(){
+    setTimeout(()=>{
+   let div =document.getElementById('contenedorcount6')
+   div.style.visibility=('visible')
+   },500)
+   }
+   function producto7(){
+    setTimeout(()=>{
+   let div =document.getElementById('contenedorcount7')
+   div.style.visibility=('visible')
+   },500)
+   }
+   function producto8(){
+    setTimeout(()=>{
+   let div =document.getElementById('contenedorcount8')
+   div.style.visibility=('visible')
+   },500)
+   }
+   function producto9(){
+    setTimeout(()=>{
+   let div =document.getElementById('contenedorcount9')
+   div.style.visibility=('visible')
+   },500)
+   }
+   function producto10(){
+    setTimeout(()=>{
+   let div =document.getElementById('contenedorcount10')
+   div.style.visibility=('visible')
+   },500)
+   }
+   function producto11(){
+    setTimeout(()=>{
+   let div =document.getElementById('contenedorcount11')
+   div.style.visibility=('visible')
+   },500)
+   }
+   function producto12(){
+    setTimeout(()=>{
+   let div =document.getElementById('contenedorcount12')
+   div.style.visibility=('visible')
+   },500)
+   }
 
 function ItemListContainer(props){
     const [itemLista, setListaItems] = useState([])
@@ -187,6 +249,27 @@ else if (productosId === '8'){
      resuelto(items.filter(carne=> carne.id===productosId))   
   },500)
 }
+else if (productosId === '9'){
+  setTimeout(()=>{
+     resuelto(items.filter(carne=> carne.id===productosId))   
+  },500)
+}
+else if (productosId === '10'){
+  setTimeout(()=>{
+     resuelto(items.filter(carne=> carne.id===productosId))   
+  },500)
+}
+else if (productosId === '11'){
+  setTimeout(()=>{
+     resuelto(items.filter(carne=> carne.id===productosId))   
+  },500)
+}
+else if (productosId === '12'){
+  setTimeout(()=>{
+     resuelto(items.filter(carne=> carne.id===productosId))   
+  },500)
+}
+
 
   else if(productosId ==='Vegano'){
    
@@ -207,6 +290,8 @@ else if (productosId === '8'){
          getPromise()
         .then(resp=>setListaItems(resp))
         .catch(err=>{console.log('alto error')}) 
+
+        
     }),[])
     return (
         <div>
